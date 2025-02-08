c6e1a089482308cc14e2e280f9944ff8819e865e
Sunil Kumar Yadav <145250590+Sunilyadav03@users.noreply.github.com>
2025-02-08 19:37:38 +0530

    Create data_preprocessing.py

---
+++ Pretrained_Models/data_preprocessing.py
@@ -0,0 +1,23 @@
+def data_preprocessing():
+  train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+    )
+
+  val_generator = val_datagen.flow_from_directory(
+    val_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+  )
+
+ test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary'
+  )
+
+ return train_generator, val_generator, test_generator
